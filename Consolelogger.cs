c238133e7d7b458103324884573e67409503f9a1
Thirumaleshb <63893075+Thirumaleshb@users.noreply.github.com>
2022-10-27 12:31:24 +0530

    ConsoleLogger class implementing ILogger Interface

---
+++ Consolelogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryRepository
+{
+    public  class Consolelogger:ILogger
+    {
+        //public StringBuilder logs=new StringBuilder();
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
