707f174bba7e5c41e92ed3a3bb4e743f5003ca3e
Thirumaleshb <63893075+Thirumaleshb@users.noreply.github.com>
2022-10-27 17:56:22 +0530

    FileLogger Class

---
+++ FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryRepository
+{
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            string path = "D:\\C#\\FileLogger5.txt";
+
+            File.AppendAllText(path, message + Environment.NewLine);
+
+        }
+    }
+}
